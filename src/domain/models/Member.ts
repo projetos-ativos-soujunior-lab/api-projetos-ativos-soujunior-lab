03fc19fc12c4ae8808a895c9ae2b21b3142ccd75
natanaelsc <natanael96@live.com>
2023-05-08 20:47:40 -0300

    feat: create Member and Project model

---
+++ src/domain/models/Member.ts
@@ -0,0 +1,15 @@
+export class Member {
+    name: string
+    github: string
+    linkedin: string
+    twitter: string
+    site: string
+
+    constructor(name: string, github: string, linkedin: string, twitter: string, site: string) {
+        this.name = name
+        this.github = github
+        this.linkedin = linkedin
+        this.twitter = twitter
+        this.site = site
+    }
+}
---
+++ src/domain/models/Project.ts
@@ -0,0 +1,17 @@
+import { Member } from "./Member"
+
+export class Project {
+    url: string
+    name: string
+    description: string
+    languages: string[]
+    members: Member[]
+
+    constructor(url: string, name: string, description: string, languages: string[], members: Member[]) {
+        this.url = url
+        this.name = name
+        this.description = description
+        this.languages = languages
+        this.members = members
+    }
+}
