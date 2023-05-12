c1cb790563bc6a5e0e77c93e9899a0a799f24113
natanaelsc <natanael96@live.com>
2023-05-11 21:04:12 -0300

    refactor: update code format with eslint and prettier

--- src/domain/models/Project.ts
+++ src/domain/models/Project.ts
@@ -1,17 +1,17 @@
-import { Member } from "./Member"
+import { type Member } from './Member';
 
 export class Project {
-    url: string
-    name: string
-    description: string
-    languages: string[]
-    members: Member[]
+  url: string;
+  name: string;
+  description: string;
+  languages: string[];
+  members: Member[];
 
-    constructor(url: string, name: string, description: string, languages: string[], members: Member[]) {
-        this.url = url
-        this.name = name
-        this.description = description
-        this.languages = languages
-        this.members = members
-    }
+  constructor(url: string, name: string, description: string, languages: string[], members: Member[]) {
+    this.url = url;
+    this.name = name;
+    this.description = description;
+    this.languages = languages;
+    this.members = members;
+  }
 }
--- src/infra/providers/LicenseProvider.ts
+++ src/infra/providers/LicenseProvider.ts
@@ -1,7 +1,7 @@
 export interface LicenseProvider {
-    key: string
-    name: string
-    spdx_id: string
-    url: string
-    node_id: string
+  key: string;
+  name: string;
+  spdx_id: string;
+  url: string;
+  node_id: string;
 }
