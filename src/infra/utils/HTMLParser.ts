d7adc1e40dfca6e4e668197ba3fe865caee80f14
natanaelsc <natanael96@live.com>
2023-05-13 18:30:26 -0300

    refactor: rename HtmlParser to HTMLParser

---
+++ src/infra/utils/HTMLParser.ts
@@ -0,0 +1,14 @@
+import { parse } from 'node-html-parser';
+
+export class HTMLParser {
+  static parse = async (link: string, selector: string, attribute: string): Promise<string> => {
+    const html = await this.getPageAsText(link);
+    const root = parse(html);
+    const element = root.querySelector(selector);
+    return element?.getAttribute(attribute) ?? '';
+  };
+
+  private static readonly getPageAsText = async (link: string): Promise<string> => {
+    return await fetch(link).then(async response => await response.text());
+  };
+}
