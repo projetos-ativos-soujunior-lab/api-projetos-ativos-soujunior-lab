ce2a49551ef9c08c6cae5a18dd5b9690eef797aa
natanaelsc <natanael96@live.com>
2023-05-13 20:07:48 -0300

    refactor: rename cache to Cache and add some more methods

---
+++ src/infra/Cache.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import NodeCache from 'node-cache';
+
+export class Cache {
+  private static cache: NodeCache;
+  private static readonly stdTTL = 1800;
+  private static readonly checkperiod = 120;
+
+  private static readonly configure = (stdTTL = this.stdTTL): NodeCache.Options => {
+    return { stdTTL, checkperiod: this.checkperiod * 0.2, useClones: false };
+  };
+
+  public static readonly has = (key: string): boolean => {
+    return this.cache.has(key);
+  };
+
+  public static readonly get = (key: string): any => {
+    console.debug('Cache Key:', key);
+    console.debug('Cache Stats:', this.cache.getStats());
+    return this.cache.get(key);
+  };
+
+  public static readonly set = (key: string, value: any, ttl = this.stdTTL): void => {
+    this.cache.set(key, value, ttl);
+  };
+
+  public static readonly clear = (): void => {
+    this.cache.flushAll();
+  };
+
+  public static readonly del = (key: string): void => {
+    this.cache.del(key);
+  };
+
+  private static readonly init = (): NodeCache => {
+    return new NodeCache(this.configure());
+  };
+
+  static {
+    this.cache = this.init();
+    console.debug(`Cache Time: ${this.stdTTL / 60} minutes`);
+  }
+}
