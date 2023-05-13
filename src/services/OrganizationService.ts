96b33eaf97758759e3d3169dcf4bbe63e66de80e
natanaelsc <natanael96@live.com>
2023-05-13 18:44:20 -0300

    feat: create OrganizationService

---
+++ src/services/OrganizationService.ts
@@ -0,0 +1,46 @@
+import { GitHub } from '../infra/api/GitHub';
+import { type Organization } from '../infra/providers/Organization';
+import { type Repository } from '../infra/providers/Repository';
+import { RepositoryService } from './RepositoryService';
+
+export class OrganizationService {
+  private readonly exclusionList: string[] = ['projetos-ativos-soujunior-lab', 'SouJunior'];
+
+  public readonly getOrganizationByName = async (name: string): Promise<Organization | undefined> => {
+    try {
+      const organization: Organization = await GitHub.api(`orgs/${name}`);
+      if (this.isOrganization(organization)) return organization;
+    } catch (e) {
+      console.error(e);
+      return undefined;
+    }
+  };
+
+  public readonly getOrganizationsByName = async (names: string[]): Promise<Organization[]> => {
+    const organizations: Organization[] = [];
+    const noRepeatOrganizations = new Set(names);
+    const namesOrganizations = this.removeOrganizations(Array.from(noRepeatOrganizations));
+    for (const name of namesOrganizations) {
+      const organization = await this.getOrganizationByName(name);
+      if (organization !== undefined) organizations.push(organization);
+    }
+    return organizations;
+  };
+
+  public readonly getOrganizationLanguages = async (repositories: Repository[]): Promise<string[]> => {
+    const repositoryService = new RepositoryService();
+    const languages: string[] = await Promise.all(
+      repositories.map(async (repository: Repository) => await repositoryService.getRepositoryLanguages(repository))
+    );
+    const allLanguages: string[] = languages.map((language: string) => language.split(', ')).flat();
+    return [...new Set(allLanguages.filter((language: string) => language !== ''))];
+  };
+
+  private readonly isOrganization = (organization: Organization): boolean => {
+    return organization.type === 'Organization';
+  };
+
+  private readonly removeOrganizations = (names: string[]): string[] => {
+    return names.filter((name: string) => !this.exclusionList.includes(name) && name !== '');
+  };
+}
