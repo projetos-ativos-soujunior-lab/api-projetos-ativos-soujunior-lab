ad3a622a78bdaba32787cfc05266a4f70cf4f766
natanaelsc <natanael96@live.com>
2023-05-13 18:41:45 -0300

    fix: add await to json response

--- src/infra/http/HttpClient.ts
+++ src/infra/http/HttpClient.ts
@@ -1,15 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export class HttpClient {
-    static get = async (url: string, token?: string): Promise<any> => {
-        try {
-            return (await fetch(url, {
-                method: 'GET',
-                headers: {
-                    Authorization: token ? `token ${token}` : '',
-                    'Content-Type': 'application/json',
-                },
-            })).json();
-        } catch (e) {
-            console.error(e);
-        }
-    };
+  static get = async (url: string, token: string): Promise<any> => {
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
+      });
+      return await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 }
