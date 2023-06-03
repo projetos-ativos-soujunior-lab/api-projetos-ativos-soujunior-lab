1eee59599035971882d6e780ff2cc83f6a48174a
natanaelsc <natanael96@live.com>
2023-06-03 15:24:19 -0300

    feat: create cron task to getting data from github api

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
-import 'dotenv/config';
 import cors from 'cors';
+import 'dotenv/config';
 import express from 'express';
+import { Cron } from './infra/Cron';
 import routes from './routes';
 
 const app = express();
+
+const task = (): void => {
+  void fetch('http://127.0.0.1:5000/api/v1/projects');
+};
+
+const cronTime = process.env.CRON_SCHEDULE as string;
+
+Cron.schedule(cronTime, task);
+
 app.use(cors());
 
 app.use('/api/v1', routes);
