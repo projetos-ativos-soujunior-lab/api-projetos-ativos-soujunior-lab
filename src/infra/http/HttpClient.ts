1eefc6f0aa206f51d2c6a130c1c0e3fa64fba366
natanaelsc <natanael96@live.com>
2023-07-25 21:10:45 -0300

    refactor: create HttpClient interface

--- src/infra/http/HttpClient.ts
+++ src/infra/http/HttpClient.ts
@@ -1,17 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export class HttpClient {
-  static get = async (url: string, token: string): Promise<any> => {
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          Authorization: token,
-          'Content-Type': 'application/json',
-        },
-      });
-      return await response.json();
-    } catch (e) {
-      console.error(e);
-    }
-  };
+export default interface HttpClient {
+  get: <T>(url: string, token?: string) => Promise<T>;
 }
