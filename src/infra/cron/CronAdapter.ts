de93e454e6b91d57f07be2cfb3eb676c1e94b1a0
natanaelsc <natanael96@live.com>
2023-07-25 21:30:36 -0300

    refactor: rename Cron to CronAdapter and create GetProjectsCron

--- src/infra/cron/CronAdapter.ts
+++ src/infra/cron/CronAdapter.ts
@@ -1,6 +1,6 @@
 import cron from 'node-cron';
 
-export class Cron {
+export default class CronAdapter {
   static readonly schedule = (cronTime: string, onTick: () => void): void => {
     cron.schedule(cronTime, onTick);
   };
