b3a4158854ebe65b4efb23518651c41025db9232
natanaelsc <natanael96@live.com>
2023-07-29 23:22:46 -0300

    feat: add default values to cache time and cron task

--- src/infra/cron/CronAdapter.ts
+++ src/infra/cron/CronAdapter.ts
@@ -1,11 +1,13 @@
 import cron from 'node-cron';
 
 export default class CronAdapter {
-  static readonly schedule = (cronTime: string, onTick: () => void): void => {
+  private static readonly cronTime: string = process.env.CRON_SCHEDULE ?? '1 */1 * * *';
+
+  static readonly schedule = (onTick: () => void, cronTime = this.cronTime): void => {
     cron.schedule(cronTime, onTick);
   };
 
   static {
-    console.debug('Cron Time:', process.env.CRON_SCHEDULE);
+    console.debug('Cron Time:', this.cronTime);
   }
 }
--- src/infra/cron/GetProjectsCron.ts
+++ src/infra/cron/GetProjectsCron.ts
@@ -11,16 +11,16 @@ export default class GetProjectsCron {
   }
 
   start = (): void => {
-    const cronTime = '*/1 * * * *';
-    CronAdapter.schedule(cronTime, async () => {
+    CronAdapter.schedule(async () => {
       const key = 'projects';
+      console.debug('Cron Job: Checking if data is in cache...');
       if (!Cache.has(key)) {
-        console.debug('Cron Job: Getting data from GitHub API...');
+        console.debug('Getting data from GitHub API...');
         const timeStart = new Date().getTime();
         const projects = await this.getProjects.execute();
         Cache.set(key, projects);
         const timeEnd = new Date().getTime();
-        console.log(`Cron Job: Getting data from GitHub API took ${timeEnd - timeStart} ms`);
+        console.log(`Getting data from GitHub API took ${timeEnd - timeStart} ms`);
       }
     });
   };
